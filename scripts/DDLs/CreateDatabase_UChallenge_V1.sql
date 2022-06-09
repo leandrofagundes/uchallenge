87c96ad036a0385bc932946ec66d94abb0c4b541
leandro.fagundes <leandro@ibrascan.com>
2022-06-09 16:06:04 -0300

    Creating scripts and files to load initial database.

---
+++ scripts/DDLs/CreateDatabase_UChallenge_V1.sql
@@ -0,0 +1,5 @@
+USE [master]
+GO
+
+CREATE DATABASE [UCHALLENGE]
+GO
---
+++ scripts/DMLs/LoadTable_FeiraLivre_V1.sql
@@ -0,0 +1,59 @@
+CREATE TABLE #temp_FeirasLivres
+(
+	[Identificador] [int] NOT NULL,
+	[Longitude] [bigint],
+	[Latitude] [bigint],
+	[SetorCensitario] [int],
+	[AreaPonderacao] [bigint],
+	[CodigoDistritoIBGE] [int],
+	[NomeDistritoMunicipal] [nvarchar](18),
+	[CodigoSubprefeitura] [int],
+	[NomeSubprefeitura] [nvarchar](25),
+	[RegiaoDivisaoEm5Areas] [nvarchar](6),
+	[RegiaoDivisaoEm8Areas] [nvarchar](7),
+	[NomeFeiraLivre] [nvarchar](30) ,
+	[RegistroFeiraLivre] [nvarchar](6) ,
+	[Logradouro] [nvarchar](34) ,
+	[Numero] [nvarchar](15) ,
+	[Bairro] [nvarchar](20) ,
+	[PontoDeReferencia] [nvarchar](30) 
+)
+GO
+
+BULK INSERT #temp_FeirasLivres
+FROM 'DEINFO_AB_FEIRASLIVRES_2014.csv'
+WITH
+(
+FORMAT = 'CSV',
+FirstRow = 2
+);
+GO
+
+insert into FEIRALIVRE
+SELECT 
+	Identificador,
+	STUFF(Longitude,4,0,'.'),
+	STUFF(Latitude,4,0,'.'),
+	SetorCensitario, 
+	AreaPonderacao,
+	CodigoDistritoIBGE,
+	NomeDistritoMunicipal,
+	CodigoSubprefeitura,
+	NomeSubprefeitura,
+	RegiaoDivisaoEm5Areas,
+	RegiaoDivisaoEm8Areas,
+	NomeFeiraLivre,
+	RegistroFeiraLivre,
+	Logradouro,
+	CASE ISNUMERIC(Numero)
+		WHEN 1 THEN CAST(Cast(numero as numeric) AS NVARCHAR(5)) 
+		ELSE 'S/N' 
+	END,
+	Bairro,
+	PontoDeReferencia 
+FROM #temp_FeirasLivres
+GO 
+
+DROP TABLE #temp_FeirasLivres
+GO
+
