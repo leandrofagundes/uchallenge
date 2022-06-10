6d8e16cc69ad296f8fe92b5fff6993a93cda59ca
Leandro de Mello Fagundes <leandro@fagundes.email>
2022-06-09 21:56:45 -0300

    Created the first domain test, just for build a path.

---
+++ src/UChallenge.Domain.Tests/Fixtures/V1Fixtures.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UChallenge.Framework.Domain.ValueObjects;
+using UChallenge.MockDB.FeiraLivreAggregates;
+
+namespace UChallenge.Domain.Tests.Fixtures
+{
+    internal sealed class V1Fixtures
+    {
+        public readonly FeiraLivre FeiraLivreVLFORMOSA;
+
+        public V1Fixtures()
+        {
+            FeiraLivreVLFORMOSA = BuildFeiraLivreVLFORMOSA();
+        }
+
+        private static FeiraLivre BuildFeiraLivreVLFORMOSA()
+        {
+            return new FeiraLivre(
+                1,
+                "VILA FORMOSA",
+                "4041-0",
+                "RUA MARAGOJIPE",
+                "S/N",
+                "VL FORMOSA",
+                "TV RUA PRETORIA",
+                87, "VILA FORMOSA",
+                new Longitude(-46.550164),
+                new Latitude(-23.558733),
+                355030885000091,
+                3550308005040,
+                26, "ARICANDUVA-FORMOSA-CARRAO",
+                "Leste", "Leste 1");
+        }
+    }
+}
---
+++ src/UChallenge.Domain.Tests/UnitTests/FeiraLivreTests/FeiraLivreUnitTests.cs
@@ -0,0 +1,36 @@
+﻿using UChallenge.Framework.Domain.ValueObjects;
+using UChallenge.MockDB.FeiraLivreAggregates;
+using Xunit;
+
+namespace UChallenge.Domain.Tests.UnitTests.FeiraLivreTests
+{
+    public sealed class FeiraLivreUnitTests
+    {
+        [Fact]
+        public void Create_FeiraLivre_ShouldSuccess()
+        {
+            // arrange
+            var feiraLivre = new FeiraLivre(
+                921,
+                "Feira Test",
+                "R-0921",
+                "Fair Avenue",
+                "921",
+                "District 9",
+                "In front of Blockbuster",
+                9,
+                "District 9",
+                new Longitude(-23.520302),
+                new Latitude(-21.003920),
+                1,
+                1,
+                1,
+                "Subcity",
+                "REGION 5",
+                "REGION 8");
+
+            // assert
+            Assert.NotNull(feiraLivre);
+        }
+    }
+}
