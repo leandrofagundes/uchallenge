6ba076ff02509adf211caf95c8f9195c69f49c1c
Leandro de Mello Fagundes <leandro@fagundes.email>
2022-06-09 22:07:04 -0300

    Created the Factory for FeiraLivre

---
+++ src/UChallenge.Domain/FeiraLivreAggregates/IFeiraLivreFactory.cs
@@ -0,0 +1,25 @@
+﻿using UChallenge.Framework.Domain.ValueObjects;
+
+namespace UChallenge.Domain.FeiraLivreAggregates
+{
+    public interface IFeiraLivreFactory
+    {
+        FeiraLivre Create(long id,
+            string nomeFeira,
+            string registroFeira,
+            string logradouro,
+            string numero,
+            string bairro,
+            string referencia,
+            long codigoDistrito,
+            string nomeDistrito,
+            Longitude longitude,
+            Latitude latitude,
+            long setorCensitario,
+            long areaDePonderacao,
+            int codigoSubPrefeitura,
+            string nomeSubPrefeitura,
+            string regiaoPorDivisaoEm5Areas,
+            string regiaoPorDivisaoEm8Areas);
+    }
+}
