576b904bd716f4239fce6e9cfc73bdb3fa481f26
Leandro de Mello Fagundes <leandro@fagundes.email>
2022-06-09 21:55:03 -0300

    Created database projects for mock data and SQL server and both model implementations.

---
+++ src/UChallenge.MockDB/FeiraLivreAggregates/FeiraLivre.cs
@@ -0,0 +1,47 @@
+﻿using UChallenge.Framework.Domain.ValueObjects;
+
+namespace UChallenge.MockDB.FeiraLivreAggregates
+{
+    public sealed record FeiraLivre :
+        Domain.FeiraLivreAggregates.FeiraLivre
+    {
+        public FeiraLivre(
+            long id,
+            string nomeFeira,
+            string registroFeira,
+            string logradouro,
+            string numero,
+            string bairro,
+            string referencia,
+            long codigoDistrito,
+            string nomeDistrito,
+            Longitude longitude,
+            Latitude latitude,
+            long setorCensitario,
+            long areaDePonderacao,
+            int codigoSubPrefeitura,
+            string nomeSubPrefeitura,
+            string regiaoPorDivisaoEm5Areas,
+            string regiaoPorDivisaoEm8Areas) :
+            base(
+                id,
+                nomeFeira,
+                registroFeira,
+                logradouro,
+                numero,
+                bairro,
+                referencia,
+                codigoDistrito,
+                nomeDistrito,
+                longitude,
+                latitude,
+                setorCensitario,
+                areaDePonderacao,
+                codigoSubPrefeitura,
+                nomeSubPrefeitura,
+                regiaoPorDivisaoEm5Areas,
+                regiaoPorDivisaoEm8Areas)
+        {
+        }
+    }
+}
