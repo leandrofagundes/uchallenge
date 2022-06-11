ed1fa008f11d3f9bb74d01e7c9dc963fbdcd21d1
Leandro de Mello Fagundes <leandro@fagundes.email>
2022-06-11 09:16:09 -0300

    Base repository and MSSQL Server DB Context was created for repositories with Microsoft  SQL Server.

---
+++ src/UChallenge.MSSQL/BaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using UChallenge.Framework.Domain.Models;
+
+namespace UChallenge.MSSQL
+{
+    public abstract class BaseRepository<TEntity, TUniqueIdentifier>
+        where TEntity : IAggregateRoot
+        where TUniqueIdentifier : struct
+    {
+        protected readonly MSSqlServerDbContext Context;
+
+        public BaseRepository(MSSqlServerDbContext context)
+        {
+            Context = context;
+        }
+    }
+}
---
+++ src/UChallenge.MSSQL/MSSqlServerDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using UChallenge.MSSQL.FeiraLivreAggregates;
+
+namespace UChallenge.MSSQL
+{
+    public sealed class MSSqlServerDbContext :
+        DbContext
+    {
+        internal DbSet<FeiraLivre> FeirasLivres { get; private set; }
+
+        public MSSqlServerDbContext(DbContextOptions options) :
+            base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            RemovePluralizingTableNameConvention(modelBuilder);
+        }
+
+        private static void RemovePluralizingTableNameConvention(ModelBuilder modelBuilder)
+        {
+            foreach (IMutableEntityType entity in modelBuilder.Model.GetEntityTypes())
+            {
+                entity.SetTableName(entity.DisplayName());
+            }
+        }
+    }
+}
