0f590015c03d8d3b762bf76d009cbc96d7acf2b0
Leandro de Mello Fagundes <leandro@fagundes.email>
2022-06-12 10:26:01 -0300

    Crated the tests for use case Create, where the domain could not be instantiated because of the rules

--- src/UChallenge.Application.Tests/UseCases/V1/FeiraLivreIntegrationTests/CreateTests.cs
+++ src/UChallenge.Application.Tests/UseCases/V1/FeiraLivreIntegrationTests/CreateTests.cs
@@ -2,6 +2,7 @@
 using UChallenge.Application.Tests.Fixtures;
 using UChallenge.Application.UseCases.V1.FeiraLivreUseCases.Create;
 using UChallenge.Framework.Application.Exceptions;
+using UChallenge.Framework.Domain.Exceptions;
 using UChallenge.Framework.Tests.Fixtures;
 using Xunit;
 
@@ -144,6 +145,71 @@ namespace UChallenge.Application.Tests.UseCases.V1.FeiraLivreIntegrationTests
             // act
             // assert
             await Assert.ThrowsAsync<BusinessApplicationInvalidInputDataException>(
+                () => useCase.RequestAsync(inputData));
+        }
+
+        [Theory]
+        [InlineData(0, "Nome", "R1234", -25.234, -25.543, 1, 1, 1, "distrito", 1, "subprefeitura", "regiao5", "regiao8", "Rua logradouro", "1", "bairro", "referencia")]
+        [InlineData(10, "", "R1234", -25.234, -25.543, 1, 1, 1, "distrito", 1, "subprefeitura", "regiao5", "regiao8", "Rua logradouro", "1", "bairro", "referencia")]
+        [InlineData(10, "Nome", "", -25.234, -25.543, 1, 1, 1, "distrito", 1, "subprefeitura", "regiao5", "regiao8", "Rua logradouro", "1", "bairro", "referencia")]
+        [InlineData(10, "Nome", "Registro", -25.123, -25.181, 0, 1, 1, "distrito", 1, "subprefeitura", "regiao5", "regiao8", "Rua logradouro", "1", "bairro", "referencia")]
+        [InlineData(10, "Nome", "Registro", -25.123, -25.181, 1, 0, 1, "distrito", 1, "subprefeitura", "regiao5", "regiao8", "Rua logradouro", "1", "bairro", "referencia")]
+        [InlineData(10, "Nome", "Registro", -25.123, -25.181, 1, 1, 0, "distrito", 1, "subprefeitura", "regiao5", "regiao8", "Rua logradouro", "1", "bairro", "referencia")]
+        [InlineData(10, "Nome", "Registro", -25.123, -25.181, 1, 1, 1, "", 1, "subprefeitura", "regiao5", "regiao8", "Rua logradouro", "1", "bairro", "referencia")]
+        [InlineData(10, "Nome", "Registro", -25.123, -25.181, 1, 1, 1, "distrito", 0, "subprefeitura", "regiao5", "regiao8", "Rua logradouro", "1", "bairro", "referencia")]
+        [InlineData(10, "Nome", "Registro", -25.123, -25.181, 1, 1, 1, "distrito", 1, "", "regiao5", "regiao8", "Rua logradouro", "1", "bairro", "referencia")]
+        [InlineData(10, "Nome", "Registro", -25.123, -25.181, 1, 1, 1, "distrito", 1, "subprefeitura", "", "regiao8", "Rua logradouro", "1", "bairro", "referencia")]
+        [InlineData(10, "Nome", "Registro", -25.123, -25.181, 1, 1, 1, "distrito", 1, "subprefeitura", "regiao5", "", "Rua logradouro", "1", "bairro", "referencia")]
+        [InlineData(10, "Nome", "Registro", -25.123, -25.181, 1, 1, 1, "distrito", 1, "subprefeitura", "regiao5", "regiao8", "", "1", "bairro", "referencia")]
+        public async Task Create_FeiraLivreUseCase_ShouldThrows_DomainFieldException(
+            long id,
+            string nomeFeira,
+            string registroFeira,
+            double latitude,
+            double longitude,
+            long setorCensitario,
+            long areaDePonderacao,
+            int codigoDistrito,
+            string nomeDistrito,
+            int codigoSubPrefeitura,
+            string nomeSubPrefeitura,
+            string regiaoPorDivisaoEm5Areas,
+            string regiaoPorDivisaoEm8Areas,
+            string logradouro,
+            string numero,
+            string bairro,
+            string referencia)
+        {
+            // arrange
+            var presenter = new CreatePresenter();
+            var useCase = new UseCase(
+                Fixtures.FeiraLivreFactory,
+                Fixtures.FeiraLivreRepository,
+                Fixtures.UnitOfWork,
+                presenter);
+
+            var inputData = new InputData(
+                id,
+                nomeFeira,
+                registroFeira,
+                longitude,
+                latitude,
+                setorCensitario,
+                areaDePonderacao,
+                codigoDistrito,
+                nomeDistrito,
+                codigoSubPrefeitura,
+                nomeSubPrefeitura,
+                regiaoPorDivisaoEm5Areas,
+                regiaoPorDivisaoEm8Areas,
+                logradouro,
+                numero,
+                bairro,
+                referencia);
+
+            // act
+            // assert
+            await Assert.ThrowsAsync<DomainFieldException>(
                 () => useCase.RequestAsync(inputData));
         }
 
